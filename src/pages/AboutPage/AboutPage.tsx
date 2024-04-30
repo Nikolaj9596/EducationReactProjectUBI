c280da27cdcbb8b91b22e21f8f11985588d3dee9
Nikolaj <nik.vagin1995@gmail.com>
2024-04-30 22:09:00 +0300

    feat: start edu react with ubi

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./AboutPage.module.scss";
+
+const AboutPage = () => {
+  return (
+    <div className={style.selectorName}>
+      About
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./MainPage.module.scss";
+
+const MainPage = () => {
+  return (
+    <div className={style.selectorName}>
+       Main
+    </div>
+  );
+};
+
+export default MainPage;
