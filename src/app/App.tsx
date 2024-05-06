56350f577f135cb41582c951f7d8c04cc176879d
Nikolaj <navagin@domclick.ru>
2024-05-06 16:45:05 +0300

    feat: added routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,7 @@
 import { classNames } from '../shared/lib/classNames/classNames';
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTheme } from './providers/ThemeProvider';
-import { AboutPage, MainPage } from '../pages';
+import AppRouter from './providers/router/ui/AppRouter';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -11,13 +10,7 @@ const App = () => {
       <button onClick={toggleTheme} >Toggle</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О нас</Link>
-      <Suspense fallback={<div>Loading....</div>}>
-        <Routes>
-          <Route path={'/'} element={<MainPage />} />
-          <Route path={'/about'} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
-
+      <AppRouter/>
     </div>
   );
 }
