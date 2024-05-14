7aed66f7b32105c04e02269a7df8745c0ff63d82
Nikolaj <navagin@domclick.ru>
2024-05-14 09:29:59 +0300

    feat: added NotFoundPage widget

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
-export { AboutPageAsync as AboutPage} from "./ui/AboutPage/AboutPage.async";
-export { MainPageAsync as MainPage} from "./ui/MainPage/MainPage.async";
+export { AboutPage} from "./AboutPage";
+export { MainPage} from "./MainPage";
+export {NotFoundPage} from "./NotFoundPage";
 
 
