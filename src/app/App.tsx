29c6ecccbe58dbbf61c85b28d9ad8e3a6afed5dd
Nikolaj.v <nikolay.v@itfox-web.com>
2024-11-27 00:11:22 +0300

    feat: added MainLayout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,36 @@
 import { classNames } from "../shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { Navbar } from "../widgets/Navbar/ui/Navbar";
-import { Sidebar } from "../widgets";
+import { PageLoader, Sidebar } from "../widgets";
 import { AppRouter } from "./providers";
 import { Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getUserInited, userActions } from "../entities/User";
 import { useSelector } from "react-redux";
+import { MainLayout } from "../shared/layouts";
 
 const App = () => {
   const { theme } = useTheme();
   document.body.className = theme;
   const dispatch = useDispatch();
-  const inited = useSelector(getUserInited)
+  const inited = useSelector(getUserInited);
 
   useEffect(() => {
-    dispatch(userActions.initAuthData()); 
-  }, [dispatch])
+    dispatch(userActions.initAuthData());
+  }, [dispatch]);
+
+  if (!inited) {
+    return <PageLoader />;
+  }
+
   return (
-   <div className={classNames("app", {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="loadding...">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          {inited && <AppRouter />}
-        </div>
+        <MainLayout
+          header={<Navbar />}
+          sidebar={<Sidebar />}
+          content={<AppRouter />}
+        />
       </Suspense>
     </div>
   );
